663696098d60755d001aa87a154e0bc062281d8c
Emji123 <indoforester7@gmail.com>
2025-08-10 10:46:45 +0700

    ubah keterangan

--- src/Keterangan.js
+++ src/Keterangan.js
@@ -12,13 +12,13 @@ const Keterangan = () => {
         <li>Nama Field diketik menggunakan huruf Kapital dan semua field harus terisi datanya, jika memang tidak ada datanya silakan isi dengan "NO DATA".</li>
         <li>Field LUAS_HA di isi dengan luasan yang sama dengan luasan rantek (maksimal lebih dari 0,5 Ha). Gunakan Proyeksi Cylindrical Equal Area (world) untuk menghitung luas polygon.</li>
         <li>
-          Shapefile dikemas dalam file berekstensi .zip dan dalam satu file zip dapat terdiri dari beberapa file shp yang berbeda (misalnya shp tahun 2019, 2020, 2022 dst dalam satu file .zip). Format penamaan file .zip dapat mengikuti format "BPDAS_JENISKEGIATAN.zip", contohnya sebagai berikut:
+          Shapefile dikemas dalam file berekstensi .zip, harap pilih nama BPDAS, tahun kegiatan dan Jenis Kegiatan terlebih dahulu. Format penamaan file .zip dapat mengikuti format "BPDAS_JENISKEGIATAN.zip", contohnya sebagai berikut:
           <ul>
             <li>"WAMPU_SEI_ULAR_RHLREGULER.zip" untuk kegiatan penanaman RHL intensif dan/atau agroforestry</li>
             <li>"WAMPU_SEI_ULAR_UPSA.zip" untuk kegiatan pembuatan dan penanaman UPSA</li>
             <li>"WAMPU_SEI_ULAR_RHLFOLU.zip" untuk kegiatan pembuatan dan penanaman RHL FOLU</li>
           </ul>
-          Penulisan nama file .shp mengikuti format "BPDAS_JENISKEGIATAN_TAHUNKEGIATAN.shp", contohnya "WAMPU_SEI_ULAR_RHLREGULER_2024.shp".
+          Penulisan nama file .shp mengikuti format "BPDAS_JENISKEGIATAN1.shp", contohnya "WAMPU_SEI_ULAR_RHLREGULER.shp".
         </li>
         <li>Hindari menempatkan file-file tersebut dalam folder maupun subfolder karena akan menyebabkan eror dalam proses upload.</li>
         <li>Shapefile wajib dibuat menggunakan proyeksi geografis datum WGS1984.</li>
